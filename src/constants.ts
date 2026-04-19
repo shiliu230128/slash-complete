42e6b8ae65d2c296dd9b297fca581f750dac904c
shiliu230128 <shiliu230128@gmail.com>
2026-04-19 21:35:29 +0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -2,144 +2,37 @@ import { SlashCompleteSettings } from "./models/slash_complete_settings";
 
 export const DEFAULT_SETTINGS: SlashCompleteSettings = {
 	hotKey: `/`,
-	commands: {
-		h1: {
-			command: "h1",
-			alias: null,
-			value: "# ",
-		},
-		h2: {
-			command: "h2",
-			alias: null,
-			value: "## ",
-		},
-		h3: {
-			command: "h3",
-			alias: null,
-			value: "### ",
-		},
-		h4: {
-			command: "h4",
-			alias: null,
-			value: "#### ",
-		},
-		h5: {
-			command: "h5",
-			alias: null,
-			value: "##### ",
-		},
-		h6: {
-			command: "h6",
-			alias: null,
-			value: "###### ",
-		},
-		bold: {
-			command: "bold",
-			alias: null,
-			value: "____",
-		},
-		italics: {
-			command: "italics",
-			alias: null,
-			value: "__",
-		},
-		"strike-through": {
-			command: "strike-through",
-			alias: null,
-			value: "~~~~",
-		},
-		subscript: {
-			command: "subscript",
-			alias: null,
-			value: "<sub></sub>",
-		},
-		super: {
-			command: "super",
-			alias: null,
-			value: "____",
-		},
-		underline: {
-			command: "underline",
-			alias: null,
-			value: "<ins></ins>",
-		},
-		highlight: {
-			command: "highlight",
-			alias: null,
-			value: "====",
-		},
-		quote: {
-			command: "quote",
-			alias: null,
-			value: ">",
-		},
-		link: {
-			command: "link",
-			alias: null,
-			value: "[]()",
-		},
-		image: {
-			command: "image",
-			alias: "img",
-			value: "![]()",
-		},
-		newline: {
-			command: "newline",
-			alias: "br",
-			value: "<br />",
-		},
-		"bullet-list": {
-			command: "bullet-list",
-			alias: "li",
-			value: "- ",
-		},
-		"number-list": {
-			command: "number-list",
-			alias: "ol",
-			value: "1. ",
-		},
-		checkbox: {
-			command: "checkbox",
-			alias: "cb",
-			value: "- [ ] ",
-		},
-		code: {
-			command: "code",
-			alias: null,
-			value: "```",
-		},
-		info: {
-			command: "info",
-			alias: null,
-			value: ">[!INFO]\n>",
-		},
-		tip: {
-			command: "tip",
-			alias: null,
-			value: ">[!TIP]\n>",
-		},
-		important: {
-			command: "important",
-			alias: null,
-			value: ">[!IMPORTANT]\n>",
-		},
-		warning: {
-			command: "warning",
-			alias: null,
-			value: ">[!WARNING]\n>",
-		},
-		caution: {
-			command: "caution",
-			alias: null,
-			value: ">[!CAUTION]\n>",
-		},
-		table: {
-			command: "table",
-			alias: "tb",
-			value: `| | | |
+	// 🔥 核心修改：把原【对象格式】改成【数组格式】（支持拖拽排序/新增/编辑）
+	commands: [
+		{ id: 1, command: "h1", alias: null, value: "# " },
+		{ id: 2, command: "h2", alias: null, value: "## " },
+		{ id: 3, command: "h3", alias: null, value: "### " },
+		{ id: 4, command: "h4", alias: null, value: "#### " },
+		{ id: 5, command: "h5", alias: null, value: "##### " },
+		{ id: 6, command: "h6", alias: null, value: "###### " },
+		{ id: 7, command: "bold", alias: null, value: "____" },
+		{ id: 8, command: "italics", alias: null, value: "__" },
+		{ id: 9, command: "strike-through", alias: null, value: "~~~~" },
+		{ id: 10, command: "subscript", alias: null, value: "<sub></sub>" },
+		{ id: 11, command: "super", alias: null, value: "____" },
+		{ id: 12, command: "underline", alias: null, value: "<ins></ins>" },
+		{ id: 13, command: "highlight", alias: null, value: "====" },
+		{ id: 14, command: "quote", alias: null, value: ">" },
+		{ id: 15, command: "link", alias: null, value: "[]()" },
+		{ id: 16, command: "image", alias: "img", value: "![]()" },
+		{ id: 17, command: "newline", alias: "br", value: "<br />" },
+		{ id: 18, command: "bullet-list", alias: "li", value: "- " },
+		{ id: 19, command: "number-list", alias: "ol", value: "1. " },
+		{ id: 20, command: "checkbox", alias: "cb", value: "- [ ] " },
+		{ id: 21, command: "code", alias: null, value: "```" },
+		{ id: 22, command: "info", alias: null, value: ">[!INFO]\n>" },
+		{ id: 23, command: "tip", alias: null, value: ">[!TIP]\n>" },
+		{ id: 24, command: "important", alias: null, value: ">[!IMPORTANT]\n>" },
+		{ id: 25, command: "warning", alias: null, value: ">[!WARNING]\n>" },
+		{ id: 26, command: "caution", alias: null, value: ">[!CAUTION]\n>" },
+		{ id: 27, command: "table", alias: "tb", value: `| | | |
 |---|---|---|
 | | | |
-| | | |`,
-		},
-	},
+| | | |` },
+	],
 };
